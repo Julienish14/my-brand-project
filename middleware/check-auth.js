fca04e13f1a11538513158a9ee1c38ed1715f6e6
Juxjulien14 <juxishimwe@gmail.com>
2022-05-11 14:06:47 +0200

    update

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -12,7 +12,11 @@ module.exports = async (req, res, next) => {
             ) {
                 token = req.headers.authorization.split(' ')[1];
             } else if (req.cookies) {
-                token = req.cookies.jwt;            }
+                token = req.cookies.jwt;            
+            }else if( req.body.jwt){
+                token = req.body.jwt;            
+
+            }
            
             const decoded =  jwt.verify(token, process.env.JWT_KEY);
 
